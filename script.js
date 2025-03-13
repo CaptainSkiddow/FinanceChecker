de9024af9849cb23263f229da724b18698719873
CaptainSkiddow <dekkerrens@outlook.com>
2025-03-13 14:54:55 +0100

    Started work on the submit form and styling

--- script.js
+++ script.js
@@ -16,28 +16,39 @@ const balanceItems = [
     { description: 'Loan', amount: '500', tag: 'liability' }
 ]
 
-flowItems.forEach(item => {
-    const li = document.createElement('li');
-    li.textContent = `${item.description}: $${item.amount}`;
 
-    if (item.tag === 'positive') {
-        incomeList.appendChild(li);
-    } else if (item.tag === 'negative') {
-        expenseList.appendChild(li);
-    }
-});
+function load_statement() {
+    flowItems.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.description}: $${item.amount}`;
 
-balanceItems.forEach(item => {
-    const li = document.createElement('li');
-    li.textContent = `${item.description}: $${item.amount}`;
-
-    if (item.tag === 'asset') {
-        assetList.appendChild(li);
-    } else if (item.tag === 'liability') {
-        liabilityList.appendChild(li);
-    }
-});
+        if (item.tag === 'positive') {
+            incomeList.appendChild(li);
+        } else if (item.tag === 'negative') {
+            expenseList.appendChild(li);
+        }
+    })
+};
 
 
+function load_balance() {
+    balanceItems.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.description}: $${item.amount}`;
+
+        if (item.tag === 'asset') {
+            assetList.appendChild(li);
+        } else if (item.tag === 'liability') {
+            liabilityList.appendChild(li);
+        }
+    })
+};
+
+
+
+
+
+load_statement();
+load_balance();
 
 console.log(flowItems);
